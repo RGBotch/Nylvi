7e40558caf7932815ddda1455c80e003975f06ce
RGBotch <romain@3prod.com>
2022-11-25 16:13:56 +0100

    prout

---
+++ nylvi/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,126 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamp('date')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('artistes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamp('date')->nullable();
+            $table->unsignedBigInteger('categorie_id')->nullable();
+            $table->foreign('categorie_id')
+                ->references('id')
+                ->on('categories');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamp('date')->nullable();
+            $table->integer('price');
+            $table->integer('size');
+            $table->unsignedBigInteger('categorie_id')->nullable();
+            $table->foreign('categorie_id')
+                ->references('id')
+                ->on('categories');
+            $table->unsignedBigInteger('artiste_id')->nullable();
+            $table->foreign('artiste_id')
+                ->references('id')
+                ->on('artistes');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('paniers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('paniers_products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('product_id')->nullable();
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products');
+            $table->integer('quantite');
+            $table->integer('price');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('collections', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamp('date')->nullable();
+            $table->string('ville')->nullable();
+            $table->unsignedBigInteger('categorie_id')->nullable();
+            $table->foreign('categorie_id')
+                ->references('id')
+                ->on('categories');
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('categorie_id')->nullable();
+            $table->foreign('categorie_id')
+                ->references('id')
+                ->on('categories');
+            $table->string('title');
+            $table->string('content');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
---
+++ nylvi/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Artiste;
+use App\Models\Categorie;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+         ]);
+
+        Categorie::factory()
+            ->state(new Sequence(
+                fn ($sequence) => [
+                    'user_id'       => User::all()->random()
+                ]
+            ))
+            ->count(50)
+            ->create();
+
+        Artiste::factory()
+            ->state(new Sequence(
+                fn ($sequence) => [
+                    'categorie_id'       => User::all()->random()
+                ]
+            ))
+            ->count(50)
+            ->create();
+
+        Product::factory()
+            ->state(new Sequence(
+                fn ($sequence) => [
+                    'artiste_id'       => Artiste::all()->random(),
+                    'categorie_id'    => Categorie::all()->random()
+                ]
+            ))
+            ->count(300)
+            ->create();
+
+    }
+}
